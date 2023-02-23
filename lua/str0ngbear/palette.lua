59e5beca0b38a93790fe9a3c0eb71e720a757dec
str0ngbear <matt@strongbear.org>
2023-02-23 18:15:41 -0500

    more pallette tweaking

--- lua/str0ngbear/palette.lua
+++ lua/str0ngbear/palette.lua
@@ -3,26 +3,34 @@ return {
 		text = '#EBDBB2';
 		text_fg = '#EBDBB2';
 		text_bg = '#1C1B1A';
-		bg0 = '#1C1B1A';
+
 		white = '#FFFFFF';
 		black = '#000000';
 		light_grey = '#BEBEBE';
 		grey = '#808080';
-			--	 '#696969',  
+
+		bg0 = '#1C1B1A';
 		bg1 = '#080808';
 		bg2 = '#343434';
 		bg3 = '#545454';
-		cerise = '#FF0033';
+
 		lime = '#AEEE00';
-		gold = '#FFB829';
-		brick = '#CB4154';
-		lilac = '#AE81FF';
-		frost = '#4db5fa' ;
+		green = '#00AB17';
+
+		yellow = '#ffea00';
 		sunny = '#FFFC7F';
-		mordant = '#AE0C00';
+
+		gold = '#FFB829';
+		orange = '#FFA500';
+
+		cerise = '#FF0000';
 		auburn = '#7C0A02';
-		moss = '#004225';
+
 		pink = '#FA4DA3';
+		lilac = '#AE81FF';
+
+		blue = '#008cff';
+		frost = '#4db5fa' ;
 
 		diff_add = '#004225';
 		diff_delete = '#7C0A02';
